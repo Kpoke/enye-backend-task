9bc5c9d2e8b997c34f4e27f673eb93ca183bee44
Olojakpoke Daniel <holodja@hotmail.com>
2021-01-27 21:00:20 +0100

    feature: add error handling

--- app.ts
+++ app.ts
@@ -3,14 +3,24 @@ import * as cors from "cors";
 import axios from "axios";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 
+app.use((error, req, res, next) => {
+  const response = {
+    status: "error",
+    message: error.message.includes("Unexpected token ")
+      ? `Invalid JSON payload passed.`
+      : `${error.message}.`,
+  };
+  res.status(error.status || 500).json(response);
+});
+
 app.get("/api/rates", async (req: express.Request, res: express.Response) => {
-  const base: string = req.query.base as string;
-  const currency: string = req.query.currency as string;
+  const base = req.query.base as string;
+  const currency = req.query.currency as string;
 
   try {
     const response = await axios.get(
@@ -25,6 +35,15 @@ app.get("/api/rates", async (req: express.Request, res: express.Response) => {
   } catch (e) {
     res.status(400).send(e.response.data);
   }
+});
+
+app.get("/*", (req, res) => {
+  const response = {
+    status: "error",
+    message: `Endpoint not found.`,
+  };
+  res.status(404);
+  res.json(response);
 });
 
 app.listen(port, () => {
